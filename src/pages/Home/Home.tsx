39b6ec8469b0f8a1b1b78e50365e18c26e09d5dc
Пользователь <uktam9527@gmail.com>
2025-11-24 10:10:42 +0500

    Khwarazm to Khwarezm

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -40,7 +40,7 @@ const Home: React.FC = () => {
                             letterSpacing: 1.5,
                         }}
                     >
-                        Journal of Khwarazm Information Technologies
+                        Journal of Khwarezm Information Technologies
                     </Typography>
 
                     <Box sx={{
